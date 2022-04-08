2597823201d055aef38d16a3f2a8537a4fc564d8
azmi72 <azmiali7221@gmail.com>
2022-04-08 13:19:32 +0700

    mod4-ses2

--- src/pages/CreatePlaylist.js
+++ src/pages/CreatePlaylist.js
@@ -1,11 +1,11 @@
+import React, { Component }  from 'react';
 import Search from "../component/CreatePlaylist/Search"
-//import { useSelector } from "react-redux";
 
 function CreatePlaylist() {
     
     return  (
         <div>
-        <Search />
+            <Search />
         </div>
     )
 }
---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch, Route  } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import CreatePlaylist from '../pages/CreatePlaylist';
+import Login from '../Login';
+
+
+function Routes() {
+    const token = useSelector((state) => state.accesstoken.value);
+    const routes = (
+        <Switch>
+            <Route path="/" exact>
+                <CreatePlaylist />
+            </Route> 
+        </Switch>
+    )
+    
+
+    return (
+        token ? routes : <Login />
+    );
+}
+
+export default Routes;
